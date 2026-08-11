3d87b403b78a6c10c6866ccbad156a044137a41c
deeepak <deeepakmaurya2001@gmail.com>
2026-08-12 05:20:08 +0530

    fixed eventlet

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, request, send_from_directory
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_cors import CORS
